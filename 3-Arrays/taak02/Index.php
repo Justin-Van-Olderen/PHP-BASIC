ef45b38603bb8a3ccb14a1461ac7cf11e8ad0403
Justin van Olderen <2107471@talnet.nl>
2020-05-12 13:51:25 +0200

    Commit 3.2 AF

---
+++ 3-Arrays/taak02/Index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+$_myDogNames_ = array("Boomer", "Jack", "CoCo");
+echo  $_myDogNames_[0] . ", " . $_myDogNames_[1] . " and " . $_myDogNames_[2] . ".";
+?>
+
+</body>
+</html>
