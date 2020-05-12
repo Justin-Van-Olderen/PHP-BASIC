6bd637bc47b496a97f3eb5ea873f10864d996dce
Justin van Olderen <2107471@talnet.nl>
2020-05-12 13:10:17 +0200

    Commit 2.9 AF

---
+++ 2-Variabelen-en-datatype/taak09/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+$_voornaam_ = "Justin";
+$_leeftijd_ ="18";
+echo "Mijn naam is $_voornaam_ en ik ben $_leeftijd_ jaar oud";
+echo "<br>";
+
+?>
+
+</body>
+</html>
