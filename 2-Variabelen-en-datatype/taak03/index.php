6ad906a9e7f27b51ae6c3bd0fd74a509d5d3cd1b
Justin van Olderen <2107471@talnet.nl>
2020-05-12 11:51:02 +0200

    commit 2.3 AF

---
+++ 2-Variabelen-en-datatype/taak03/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+$_Leeftijd_ = "18";
+$_melk_ ="1.05";
+echo $_Leeftijd_;
+echo "<br>";
+echo $_melk_;
+echo "<br>";
+?>
+
+</body>
+</html>
