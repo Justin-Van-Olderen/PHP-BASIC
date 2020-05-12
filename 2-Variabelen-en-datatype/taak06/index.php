21d4abea087750cf13dc42bf8da0b1ffb5b71262
Justin van Olderen <2107471@talnet.nl>
2020-05-12 12:38:06 +0200

    commit 2.6 AF

---
+++ 2-Variabelen-en-datatype/taak06/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+$_string_ = "string";
+$integer = 5985;
+$float = 10.365;
+$boolean = true;
+$null = null;
+echo $_string_;
+echo "<br>";
+var_dump($integer);
+echo "<br>";
+var_dump($float);
+echo "<br>";
+echo gettype($boolean);
+echo "<br>";
+var_dump($null);
+?>  
+
+
+
+
+</body>
+</html>
