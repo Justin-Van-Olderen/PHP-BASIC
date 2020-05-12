52b58cac9e3b734bc3b8a76d1ccbb80f986fb8c7
Justin van Olderen <2107471@talnet.nl>
2020-05-12 13:07:57 +0200

    commit 2.8 AF

---
+++ 2-Variabelen-en-datatype/taak08/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+$_voornaam1_ = "Max";
+$_voornaam2_ = "Justin";
+$_voornaam3_ = "Sander";
+echo $_voornaam1_;
+echo "<br>";
+echo $_voornaam2_;
+echo "<br>";
+echo $_voornaam3_;
+?>
+
+</body>
+</html>
