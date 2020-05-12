5b53b2b58492eb59bd681ed6b913cd5b93a770df
Justin van Olderen <2107471@talnet.nl>
2020-05-12 14:14:17 +0200

    Commit 4.2 AF

---
+++ 4-Arithmetics/taak02/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+$_getal1_ = 420;
+$_getal2_ = 69;
+
+Echo "Dit is getal 1:  $_getal1_"; 
+echo "<br>";
+echo "Dit is getal 2:  $_getal2_"; 
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+
+echo "<br>";
+echo $_getal1_ += $_getal2_;
+echo "<br>";
+echo $_getal1_ -= $_getal2_;
+echo "<br>";
+echo $_getal1_ *= $_getal2_;
+echo "<br>";
+echo $_getal1_ /= $_getal2_;
+echo "<br>";
+?>  
+
+</body>
+</html>
