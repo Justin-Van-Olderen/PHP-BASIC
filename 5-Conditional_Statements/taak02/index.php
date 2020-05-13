4c9bb1d1ac0083fdf9d1b2194508e28d71e55269
Justin van Olderen <2107471@talnet.nl>
2020-05-13 11:12:21 +0200

    Commit 5.2

---
+++ 5-Conditional_Statements/taak02/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+$_test_1 = 10;
+$_test_2 = 11;
+
+if ($_test_1 == $_test_2){
+    echo "gelijk";
+    echo "<br>";
+}
+
+else{
+    echo "ongelijk";
+    echo "<br>";
+}
+
+if ($_test_1 < $_test_2){
+    echo "identiek";
+    echo "<br>";
+}
+
+else {
+    echo "ongelijk";
+    echo "<br>";
+}
+
+if ($_test_1 > $_test_2){
+echo "test 1 is groter dan teks 2";
+echo "<br>";
+}
+
+
+?>
+
+</body>
+</html>
